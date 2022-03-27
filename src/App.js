0367070e422c3f86fa923b3c63c5af9527cf5766
eu <eugen_hhh@yahoo.com>
2022-03-27 14:23:49 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// function logRandom(){
+  //	console.log(Math.random());
+// }
+
+function Button(props) {
+  const handleButton = ()=>props.handleClick(props.increment)
+	return <button onClick={handleButton}>+{props.increment}</button>;
 }
+
+function Display(props) {
+	return <div>{props.message}</div>;
+}
+
+function App(props) {
+  const [counter,setCounter]=useState(5);
+  const incrementCounter=(incrementValue)=>setCounter(counter+incrementValue);
+	return <div>
+        <Button handleClick={incrementCounter} increment={1}></Button>
+    <Button handleClick={incrementCounter} increment={2}></Button>
+    <Button handleClick={incrementCounter} increment={3}></Button>
+    <Button handleClick={incrementCounter} increment={5}></Button>
+        <Display message={counter}></Display>
+    </div>;
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('mountNode'),
+);
 
 export default App;
